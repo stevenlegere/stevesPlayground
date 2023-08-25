110b5bf693593e8c2e4f55b87415d0d9c496d283
stevenlegere <stevenmarklegere@gmail.com>
2023-08-25 21:27:54 +0100

    function practice

--- index.js
+++ index.js
@@ -35,3 +35,18 @@ function journalEvents(journal) {
 }
 console.log(journalEvents(JOURNAL));
 // → ["carrot", "exercise", "weekend", "bread", …]
+
+function phi(table) {
+    return (table[3] * table[0] - table[2] * table[1]) /
+        Math.sqrt((table[2] + table[3]) *
+            (table[0] + table[1]) *
+                (table[1] + table[3]) *
+                    (table[0] + table[2]));
+}
+console.log(phi([76, 9, 4, 1]));
+// → 0.068599434
+
+
+for (let event of journalEvents(JOURNAL)) {
+    console.log(event + ":", phi(tableFor(event, JOURNAL)));
+}
