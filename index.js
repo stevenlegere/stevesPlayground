5592e7123b3c04a390aac743435a4c679bea5547
stevenlegere <stevenmarklegere@gmail.com>
2023-09-21 22:01:39 +0100

    Working on deepEqual and Abstraction

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ function phi(table) {
     return (table[3] * table[0] - table[2] * table[1]) /
         Math.sqrt((table[2] + table[3]) *
             (table[0] + table[1]) *
-                (table[1] + table[3]) *
-                    (table[0] + table[2]));
+            (table[1] + table[3]) *
+            (table[0] + table[2]));
 }
 console.log(phi([76, 9, 4, 1]));
 // → 0.068599434
@@ -53,7 +53,7 @@ for (let event of journalEvents(JOURNAL)) {
 
 // Iterates through events in the JOURNAL, calculates the phi coefficient for each event's presence, and then logs events with a correlation greater than or less than 0.1 along with their correlation values.
 for (let event of journalEvents(JOURNAL)) {
-    let correlation = phi (tableFor(event, JOURNAL));
+    let correlation = phi(tableFor(event, JOURNAL));
     if (correlation > 0.1 || correlation < 0.1) {
         console.log(event + ":", correlation);
     }
@@ -62,28 +62,29 @@ for (let event of journalEvents(JOURNAL)) {
 // Searches through entries in the JOURNAL for occurrences where "peanuts" were consumed but "brushed teeth" was not, adds "peanut teeth" as an event to those entries, and then calculates and logs the phi coefficient for the "peanut teeth" event, which is equal to 1.
 for (let entry of JOURNAL) {
     if (entry.events.includes("peanuts") &&
-    !entry.events.includes("brushed teeth"))
-    {
+        !entry.events.includes("brushed teeth")) {
         entry.events.push("peanut teeth")
     }
 }
 console.log(phi(tableFor("peanut teeth", JOURNAL)));
 // 1
 
-function phi([ n00, n01, n10, n11 ]) {
+function phi([n00, n01, n10, n11]) {
     return (n11 * n00 - n10 * n01) /
-    Math.sqrt((n10 + n11) * (n00 + n01) *
-    (n01 + n11) * (n00 * n10));
+        Math.sqrt((n10 + n11) * (n00 + n01) *
+            (n01 + n11) * (n00 * n10));
 }
 const input = [10, 5, 3, 12];
 const result = phi(input);
 console.log(`phi coefficient for input [n00=${input[0]}, n01=${input[2]}, n11=${input[3]}]: ${result}`);
 
-let {name} = {name: "Faraji", age: 23};
+let { name } = { name: "Faraji", age: 23 };
 console.log(name);
 
-let string = JSON.stringify({squirrel: false,
-    events: ["weekend"]});
+let string = JSON.stringify({
+    squirrel: false,
+    events: ["weekend"]
+});
 console.log(string);
 // → {"squirrel":false,"events":["weekend"]}
 console.log(JSON.parse(string).events);
@@ -103,17 +104,17 @@ let list = {
 console.log(list);
 
 // Write a functiom arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument
-function arrayToListArray (array) {
+function arrayToListArray(array) {
     let list = null;
     for (let i = array.length - 1; i >= 0; i--) {
-        list = {value: array[i], rest: list};
+        list = { value: array[i], rest: list };
     }
     return list;
 }
 console.log(arrayToListArray([1, 2, 3]));
 
 // Write a function listToArray that produces an array from a list
-function listToArrayList (list) {
+function listToArrayList(list) {
     let array = [];
     for (let node = list; node; node = node.rest) {
         array.push(node.value);
@@ -121,3 +122,34 @@ function listToArrayList (list) {
     return array;
 }
 console.log(listToArrayList(arrayToListArray([1, 2, 3])));
+
+// Deep Comparison
+// Write a function deepEqual that takes two values and returns true only if they are the same value or are objects with the same properties, where the values of the properties are equal when compared with a recursive call to deepEqual
+function deepEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || typeof a != "object" ||
+        b == null || typeof b != "object") return false;
+    let keysA = Object.keys(a), keysB = Object.keys(b);
+    if (keysA.length != keysB.length) return false;
+    for (let key of keysA) {
+        if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+    }
+    return true;
+}
+console.log(
+    deepEqual(
+      { value: 1, rest: { value: 2, rest: { value: 3, rest: null } } },
+      { value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }
+    )
+);
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+function repeatLog(n) {
+    for (let i = 0; i < n; i++) {
+        console.log(i);
+    }
+}
+repeatLog(10);
