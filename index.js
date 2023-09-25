6440209801c54f106214af5a3c8634f787d2ab17
stevenlegere <stevenmarklegere@gmail.com>
2023-09-25 21:56:50 +0100

    Higher Order Functions

--- index.js
+++ index.js
@@ -171,3 +171,31 @@ repeat(5, i => {
 console.log(labels);
 // → ["Unit 1", "Unit 2", "Unit 3", "Unit 4", "Unit 5"]
 
+// Higher-Order Functions
+function greaterThan(n) {
+    return m => m > n;
+}
+let greaterThan10 = greaterThan(10);
+console.log(greaterThan10(11));
+// → true
+
+function noisy(f) {
+    return (...args) => {
+        console.log("calling with", args);
+        let result = f(...args);
+        console.log("called with", args, ", returned", result);
+        return result;
+    };
+}
+noisy(Math.min)(3, 2, 1);
+// → calling with [3, 2, 1]
+// → called with [3, 2, 1] , returned 1
+
+function unless(test, then) {
+    if (!test) then();
+}
+repeat(3, n => {
+    unless(n % 2 == 1, () => {
+        console.log(n, "is even");
+    });
+});
