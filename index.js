0675497c40958edfb76d7daa105f182911e1a35c
stevenlegere <stevenmarklegere@gmail.com>
2023-11-05 21:15:03 +0000

    Destructuring assignments

--- index.js
+++ index.js
@@ -1137,3 +1137,18 @@ removeFirstTwo = (list) => {
 const sourceWithoutFirstTwo = removeFirstTwo(source);
 console.log(sourceWithoutFirstTwo); // [3, 4, 5, 6, 7, 8, 9, 10]
 
+// destructuring assignment within the argument to the function half to send only max and min inside the function
+const stats = {
+    max: 56.78,
+    standar_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+const half = ({ max, min }) => {
+    return (max + min) / 2.0;
+}
+
+console.log(half(stats)); // 28.015
+
