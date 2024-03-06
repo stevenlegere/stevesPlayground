bc7fc60323621ea906a965dde8acf2158463e425
stevenlegere <stevenmarklegere@gmail.com>
2024-03-06 21:32:04 +0000

    schl grades

--- Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
+++ Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using System; // using statement enables you to write code that implements members of the System namespace without requiring you to specify System
 
 // initialize variables - graded assignments 
-int currentAssignments = 5;
+int currentAssignments = 5; // variable, currentAssignments is used to hold the number of exams that have been scored
 
+// create an integer array that will be used for Sophia's assignment scores
+int[] sophiaScores = new int[5]; // the new operator is to specify that you're creating a new instance of an array
+
+// variables that are used to hold the scores of graded assignments
 int sophia1 = 90;
 int sophia2 = 86;
 int sophia3 = 87;
@@ -27,21 +31,26 @@ int logan3 = 87;
 int logan4 = 88;
 int logan5 = 96;
 
+// These variables are integers that are being used to hold the sum of the exam scores.
 int sophiaSum = 0;
 int andrewSum = 0;
 int emmaSum = 0;
 int loganSum = 0;
 
+// These variables are decimals that are used to hold the calculated average score.
 decimal sophiaScore;
 decimal andrewScore;
 decimal emmaScore;
 decimal loganScore;
 
+// These equations is used to calculate the sum of the assignment scores for each student.
 sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
 andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
 emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
 loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
 
+// These equations calculates the average score
+    // The numerator is cast as a decimal to ensure the division retains the fractional component
 sophiaScore = (decimal)sophiaSum / currentAssignments;
 andrewScore = (decimal)andrewSum / currentAssignments;
 emmaScore = (decimal)emmaSum / currentAssignments;
@@ -54,4 +63,5 @@ Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
 Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
 
 Console.WriteLine("Press the Enter key to continue");
-Console.ReadLine();
+Console.ReadLine(); // This will pause the program until the user presses the Enter key
+
