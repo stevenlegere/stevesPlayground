ab97f9f3b20fc21bf98b4ed43f44dd1e24829076
stevenlegere <stevenmarklegere@gmail.com>
2024-03-08 19:50:29 +0000

    exam scores

--- Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
+++ Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -11,34 +11,25 @@ int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
 // These variables are integers that are being used to hold the sum of the exam scores.
 int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
+
 
 // These variables are decimals that are used to hold the calculated average score.
 decimal sophiaScore;
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
 
-// These equations is used to calculate the sum of the assignment scores for each student.
-sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
-loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
+foreach (int score in sophiaScores)
+{
+    // add the exam score to the sum
+    sophiaSum += score;
+}
 
 // These equations calculates the average score
     // The numerator is cast as a decimal to ensure the division retains the fractional component
 sophiaScore = (decimal)sophiaSum / currentAssignments;
-andrewScore = (decimal)andrewSum / currentAssignments;
-emmaScore = (decimal)emmaSum / currentAssignments;
-loganScore = (decimal)loganSum / currentAssignments;
+
 
 Console.WriteLine("Student\t\tGrade\n");
 Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine(); // This will pause the program until the user presses the Enter key
