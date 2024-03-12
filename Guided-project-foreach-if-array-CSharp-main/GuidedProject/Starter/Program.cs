843e1297c6298fb335cd530dae03d4f4c487f8c9
stevenlegere <stevenmarklegere@gmail.com>
2024-03-12 20:01:16 +0000

    Student grades in C#

--- Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
+++ Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -1,18 +1,20 @@
 ﻿using System; // using statement enables you to write code that implements members of the System namespace without requiring you to specify System
 
 // initialize variables - graded assignments 
-int currentAssignments = 5; // variable, currentAssignments is used to hold the number of exams that have been scored
+int examAssignments = 5; // variable, currentAssignments is used to hold the number of exams that have been scored
 
 // create an integer array that will be used for Sophia's assignment scores
-int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 }; // the new operator is to specify that you're creating a new instance of an array
-int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
-int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
-int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 }; // the new operator is to specify that you're creating a new instance of an array
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
 
 // Student names
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
 int[] studentScores = new int[10]; // This creates an array of integers that can hold 10 values
+
+string currentStudentLetterGrade = ""; // This creates a string variable that will be used to hold the letter grade for the current student
 
 Console.WriteLine("Student\t\tGrade\n");
 
@@ -38,6 +40,7 @@ foreach (string name in studentNames)
     // These variables are decimals that are used to hold the calculated average score.
     decimal currentStudentGrade;
 
+    int gradedAssignments = 0; // // initialize/reset a counter for the number of assignments
 
     foreach (int score in studentScores)
     {
@@ -48,10 +51,48 @@ foreach (string name in studentNames)
 
     // These equations calculates the average score
     // The numerator is cast as a decimal to ensure the division retains the fractional component
-    currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+    currentStudentGrade = (decimal)sumAssignmentScores / examAssignments;
 
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
 
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
 
 
 }
